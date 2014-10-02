a8f19053e7bf5e2be6881a7bf72b8ed0506db996
Ivan <ivanlopes@42algoritmos.com.br>
2014-10-02 07:17:29 -0300

    Update

---
+++ matriz_inversa.m
@@ -0,0 +1,13 @@
+(* USAGE:                  *)
+(* math < matriz_inversa.m *)
+(*                         *)
+
+(* matriz A *)
+A = {{2, 3}, {1, -1}}
+
+(* matriz B *)
+B = Inverse[A]
+
+
+(* matriz I *)
+A.B
